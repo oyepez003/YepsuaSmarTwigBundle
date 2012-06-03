71eeed783c784009d725cdb917ecf6c3295daf4f
Omar Yepez <omar.yepez@yepsua.com>
2012-06-02 23:34:59 -0430

    [Fix] configureHTMLProperties redeclaration

--- Twig/Node/DynamicSelectNode.php
+++ Twig/Node/DynamicSelectNode.php
@@ -7,12 +7,8 @@ class DynamicSelectNode extends SimpleNode {
   public function getWidgetName(){
     return 'dynaselect';
   }
-
-  public function configureHTMLProperties(){
-    return array('disabled','multiple','name','size');
-  }
   
   public function configureHTMLProperties(){
-    return $this->getHTMLAttrs('input','select');
+    return $this->getHTMLAttrs('select');
   }
 }
