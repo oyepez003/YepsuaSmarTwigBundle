f9bc0b7f2138a8265fa7dfb4657a3d258dd1bef8
Omar Yepez <oyepez003@gmail.com>
2014-03-12 17:36:12 -0430

    [update] Last stable version.

--- UI/Message/Notification.php
+++ UI/Message/Notification.php
@@ -6,30 +6,28 @@ use \YsJQuery as JQuery;
 use \YsPNotify as PNotify;
 use \YsJQueryConstant as JQueryConstant;
 
+JQuery::usePlugin(JQueryConstant::PLUGIN_PNOTYFY);
+
 /**
  * Description of Notification
  *
  * @author oyepez
  */
 class Notification{
-  
+    
   public static function notice($content){
-    JQuery::usePlugin(JQueryConstant::PLUGIN_PNOTYFY);
     return self::build(PNotify::notice($content));
   }
     
   public static function error($content){
-    JQuery::usePlugin(JQueryConstant::PLUGIN_PNOTYFY);
     return self::build(PNotify::error($content));
   }
   
   public static function say($content){
-    JQuery::usePlugin(JQueryConstant::PLUGIN_PNOTYFY);
     return self::build(PNotify::say($content));
   }
   
   public static function alarm($content){
-    JQuery::usePlugin(JQueryConstant::PLUGIN_PNOTYFY);
     return self::build(PNotify::alarm($content));
   }
     
